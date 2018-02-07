2aa28f30ee3f5643a9cacd497fbfa88e9b313a81
mihanc <33028054+mihanc@users.noreply.github.com>
2018-02-07 15:56:21 +0300

    slider modify

--- src/js/main.js
+++ src/js/main.js
@@ -27,7 +27,33 @@ $(document).ready(function(){
         appendArrows: $('.arrow'),
 
         prevArrow: '<button id="prev" type="button" class="btn btn-juliet"> </button>',
-        nextArrow: '<button id="next" type="button" class="btn btn-juliet"></button>'
+        nextArrow: '<button id="next" type="button" class="btn btn-juliet"></button>',
+
+        responsive: [
+
+            {
+                breakpoint: 1025,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 426,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+
+        ],
 
     });
 
