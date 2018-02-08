16913676d5f9eb9b7d2265a422b621638d977cbf
mihanc <33028054+mihanc@users.noreply.github.com>
2018-02-08 11:24:06 +0300

    added hamburger menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,28 @@
 $(document).ready(function(){
 
-    $('.cycle').slick({
+    $( ".cross" ).hide();
+    $( ".menu" ).hide();
+    $( ".hamburger" ).click(function() {
+        $( ".menu" ).slideToggle( "slow", function() {
+            $( ".hamburger" ).hide();
+            $( ".cross" ).show();
+        });
+    });
+
+    $( ".cross" ).click(function() {
+        $( ".menu" ).slideToggle( "slow", function() {
+            $( ".cross" ).hide();
+            $( ".hamburger" ).show();
+        });
+    });
+
+    $('.team-slider').slick({
         infinite: true,
         speed: 150,
 
-        slidesToShow: 1,
-
-        slidesToScroll: 1,
-
-        appendArrows: $('.arrow'),
-
-        prevArrow: '<button id="prev" type="button" class="btn btn-juliet"> </button>',
-        nextArrow: '<button id="next" type="button" class="btn btn-juliet"></button>'
-
-    });
-
-
-    $('.cycle2').slick({
-        infinite: true,
-        speed: 350,
-
         slidesToShow: 4,
 
-        slidesToScroll: 3,
+        slidesToScroll: 1,
 
         appendArrows: $('.arrow'),
 
@@ -35,7 +35,7 @@ $(document).ready(function(){
                 breakpoint: 1025,
                 settings: {
                     slidesToShow: 3,
-                    slidesToScroll: 1
+                    slidesToScroll: 1,
                 }
             },
             {
@@ -57,5 +57,64 @@ $(document).ready(function(){
 
     });
 
+
+    $('.blockquote-slider').slick({
+        infinite: true,
+        speed: 150,
+
+        slidesToShow: 1,
+
+        slidesToScroll: 1,
+
+        appendArrows: $('.arrow'),
+
+        prevArrow: '<button id="prev" type="button" class="btn btn-juliet"> </button>',
+        nextArrow: '<button id="next" type="button" class="btn btn-juliet"></button>',
+
+    });
+
+
+
+
+    $('.cycle2').slick({
+        infinite: true,
+        speed: 350,
+
+        slidesToShow: 4,
+
+        slidesToScroll: 3,
+        lazyLoad: 'ondemand',
+        appendArrows: $('.arrow'),
+
+        prevArrow: '<button id="prev" type="button" class="btn btn-juliet"> </button>',
+        nextArrow: '<button id="next" type="button" class="btn btn-juliet"></button>',
+
+        responsive: [
+
+            {
+                breakpoint: 1025,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 769,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 426,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+
+        ],
+
+    });
 
 });
