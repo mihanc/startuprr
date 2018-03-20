3547cf6e986fc555c31afe3ead723b190dfa485f
misha <nimchenko2013@mail.ru>
2018-03-20 23:48:03 +0300

    generated svg-sprites

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,11 @@ var gulp          = require('gulp'),
     scss          = require('gulp-sass'),
     cleanCSS      = require('gulp-clean-css'),
     pug           = require('gulp-pug'),
+    svgSprite     = require('gulp-svg-sprite'),
+    plumber       = require('gulp-plumber'),
+    svgo          = require('gulp-svgo'),
+    cheerio       = require('gulp-cheerio'),
+    replace       = require('gulp-replace'),
     concat        = require('gulp-concat'),
     csscomb       = require('gulp-csscomb');
     autoprefixer  = require('gulp-autoprefixer'),
@@ -13,6 +18,7 @@ var gulp          = require('gulp'),
     paths   = {
         js          : './src/js/',
         images      : './src/images/',
+        svg         : './src/svg/',
         fonts       : './src/fonts/',
         css         : './build/css/',
         scss        : './src/styles/',
@@ -48,6 +54,59 @@ var gulp          = require('gulp'),
         }
     };
 
+
+gulp.task('svg', function() {
+    return gulp.src(paths.svg + '*.svg')
+        .pipe(plumber())
+        // minify svg
+        //.pipe(svgmin({
+        //  js2svg: {
+        //    pretty: true
+        //  }
+        //}))
+        .pipe(svgo({
+            js2svg: {
+                indent: 2, // optional, default is 4
+                pretty: true
+            }
+        }))
+        // remove all fill and style declarations in out shapes
+        .pipe(cheerio({
+            run:           function($) {
+                $('[fill]').removeAttr('fill');
+                $('[stroke]').removeAttr('stroke');
+                $('[style]').removeAttr('style');
+            },
+            parserOptions: {xmlMode: true}
+        }))
+        // cheerio plugin create unnecessary string '&gt;', so replace it.
+        .pipe(replace('&gt;', '>'))
+        // build svg sprite
+        .pipe(svgSprite({
+            mode: {
+                symbol: {
+                    sprite:  '../sprite.svg',
+                    render:  {
+                        scss: {
+                            dest:'../../styles/svg/_svg-sprite.scss',
+                            template: paths.scss + "templates/_sprite_template.scss"
+                        }
+                    },
+                    example: true
+                }
+            }
+        }))
+        .pipe(replace('<?xml version="1.0" encoding="utf-8"?>', ''))
+        .pipe(cheerio({
+            run:           function($) {
+                $('[xmlns]').removeAttr('xmlns');
+                $('svg').css('display', 'none');
+            },
+            parserOptions: {xmlMode: true}
+        }))
+        .pipe(plumber.stop())
+        .pipe(gulp.dest(paths.images));
+});
 
 gulp.task('compress', function() {
     gulp.src('./src/images/**/*')
